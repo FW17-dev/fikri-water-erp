436e95170e987fd4d32c43e534d2fc92bf6ea6e3
FW17-dev <fikri.water17@gmail.com>
2026-07-01 04:01:30 +0800

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,7 +5,8 @@ use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Request;
 
-$app = Application::configure(basePath: dirname(__DIR__))
+return Application::configure(basePath: dirname(__DIR__))
+    ->withProviders()
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
@@ -18,6 +19,5 @@ $app = Application::configure(basePath: dirname(__DIR__))
         $exceptions->shouldRenderJsonWhen(
             fn (Request $request) => $request->is('api/*'),
         );
-    })->create();
-
-return $app;
+    })
+    ->create();
