7ff49eed98f64bf4aa47f32d158a7f142fa63d7f
FW17-dev <fikri.water17@gmail.com>
2026-06-30 23:27:21 +0700

    Update index.php

--- api/index.php
+++ api/index.php
@@ -2,4 +2,7 @@
 
 define('LARAVEL_START', microtime(true));
 
+$_SERVER['SCRIPT_FILENAME'] = __DIR__ . '/../public/index.php';
+$_SERVER['SCRIPT_NAME'] = '/index.php';
+
 require __DIR__ . '/../public/index.php';
