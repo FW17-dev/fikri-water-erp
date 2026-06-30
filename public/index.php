a8bcfdf3223d9b631e179ea21a32558e7c6b74f1
FW17-dev <fikri.water17@gmail.com>
2026-07-01 02:34:11 +0700

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,32 +1,30 @@
 <?php
 
-use Illuminate\Foundation\Application;
-use Illuminate\Http\Request;
-
 define('LARAVEL_START', microtime(true));
 
-// Determine if the application is in maintenance mode...
-if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
-    require $maintenance;
-}
-
-// Register the Composer autoloader...
 require __DIR__.'/../vendor/autoload.php';
 
-// Bootstrap Laravel and handle the request...
-/** @var Application $app */
-$app = require_once __DIR__.'/../bootstrap/app.php';
-
 try {
-    $app->handleRequest(Request::capture());
+
+    /** @var \Illuminate\Foundation\Application $app */
+    $app = require_once __DIR__.'/../bootstrap/app.php';
+
+    $app->handleRequest(Illuminate\Http\Request::capture());
+
 } catch (\Throwable $e) {
+
     http_response_code(500);
 
     header('Content-Type: text/plain');
 
-    echo "REAL ERROR\n\n";
-    echo $e->getMessage() . "\n\n";
-    echo $e->getFile() . ":" . $e->getLine() . "\n\n";
+    echo "===== REAL ERROR =====\n\n";
+
+    echo $e::class."\n\n";
+
+    echo $e->getMessage()."\n\n";
+
+    echo $e->getFile().":".$e->getLine()."\n\n";
+
     echo $e->getTraceAsString();
 
     exit;
