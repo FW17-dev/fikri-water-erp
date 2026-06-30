6ca00f318751bb2f2a5554b283f59ce2d3ed881d
FW17-dev <fikri.water17@gmail.com>
2026-07-01 04:00:04 +0800

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,31 +1,17 @@
 <?php
 
+use Illuminate\Foundation\Application;
+use Illuminate\Http\Request;
+
 define('LARAVEL_START', microtime(true));
+
+if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+    require $maintenance;
+}
 
 require __DIR__.'/../vendor/autoload.php';
 
-try {
+/** @var Application $app */
+$app = require_once __DIR__.'/../bootstrap/app.php';
 
-    /** @var \Illuminate\Foundation\Application $app */
-    $app = require_once __DIR__.'/../bootstrap/app.php';
-
-    $app->handleRequest(Illuminate\Http\Request::capture());
-
-} catch (\Throwable $e) {
-
-    http_response_code(500);
-
-    header('Content-Type: text/plain');
-
-    echo "===== REAL ERROR =====\n\n";
-
-    echo $e::class."\n\n";
-
-    echo $e->getMessage()."\n\n";
-
-    echo $e->getFile().":".$e->getLine()."\n\n";
-
-    echo $e->getTraceAsString();
-
-    exit;
-}
+$app->handleRequest(Request::capture());
