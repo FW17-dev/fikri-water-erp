3398886484270a6c14f8aad4c0c0ad3f90e109d0
FW17-dev <fikri.water17@gmail.com>
2026-07-01 02:24:36 +0700

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,6 +1,24 @@
 <?php
 
-$_SERVER['SCRIPT_FILENAME'] = __DIR__ . '/../public/index.php';
-$_SERVER['SCRIPT_NAME'] = '/index.php';
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
 
-require_once __DIR__ . '/../public/index.php';
+try {
+    $_SERVER['SCRIPT_FILENAME'] = __DIR__ . '/../public/index.php';
+    $_SERVER['SCRIPT_NAME'] = '/index.php';
+
+    require_once __DIR__ . '/../public/index.php';
+} catch (Throwable $e) {
+    http_response_code(500);
+
+    echo "<h2>VERCEL RUNTIME ERROR</h2>";
+    echo "<pre>";
+    echo $e->getMessage();
+    echo "\n\n";
+    echo $e->getFile();
+    echo " : ";
+    echo $e->getLine();
+    echo "\n\n";
+    echo $e->getTraceAsString();
+    echo "</pre>";
+}
